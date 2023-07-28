cc9e2ecf12cd8801e8a6227483818c22c38c1698
Kai Bellmann <kb@bellmann-engineering.com>
2023-07-28 14:42:36 +0200

    changed title of main window

--- IISLogFileViewer/Form1.Designer.vb
+++ IISLogFileViewer/Form1.Designer.vb
@@ -22,48 +22,53 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        btnOpenFile = New Button()
-        txtFile = New TextBox()
-        lvEntries = New ListView()
-        SuspendLayout()
-        ' 
-        ' btnOpenFile
-        ' 
-        btnOpenFile.Location = New Point(713, 12)
-        btnOpenFile.Name = "btnOpenFile"
-        btnOpenFile.Size = New Size(75, 23)
-        btnOpenFile.TabIndex = 0
-        btnOpenFile.Text = "..."
-        btnOpenFile.UseVisualStyleBackColor = True
-        ' 
-        ' txtFile
-        ' 
-        txtFile.Location = New Point(12, 12)
-        txtFile.Name = "txtFile"
-        txtFile.ReadOnly = True
-        txtFile.Size = New Size(695, 23)
-        txtFile.TabIndex = 1
-        ' 
-        ' lvEntries
-        ' 
-        lvEntries.Location = New Point(12, 41)
-        lvEntries.Name = "lvEntries"
-        lvEntries.Size = New Size(776, 397)
-        lvEntries.TabIndex = 2
-        lvEntries.UseCompatibleStateImageBehavior = False
-        ' 
-        ' Form1
-        ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
-        AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(lvEntries)
-        Controls.Add(txtFile)
-        Controls.Add(btnOpenFile)
-        Name = "Form1"
-        Text = "IIS Log File Viewer"
-        ResumeLayout(False)
-        PerformLayout()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        Me.lvEntries = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(1324, 26)
+        Me.btnOpenFile.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(139, 49)
+        Me.btnOpenFile.TabIndex = 0
+        Me.btnOpenFile.Text = "..."
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'txtFile
+        '
+        Me.txtFile.Location = New System.Drawing.Point(22, 26)
+        Me.txtFile.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.ReadOnly = True
+        Me.txtFile.Size = New System.Drawing.Size(1287, 39)
+        Me.txtFile.TabIndex = 1
+        '
+        'lvEntries
+        '
+        Me.lvEntries.Location = New System.Drawing.Point(22, 87)
+        Me.lvEntries.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.lvEntries.Name = "lvEntries"
+        Me.lvEntries.Size = New System.Drawing.Size(1438, 842)
+        Me.lvEntries.TabIndex = 2
+        Me.lvEntries.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1486, 960)
+        Me.Controls.Add(Me.lvEntries)
+        Me.Controls.Add(Me.txtFile)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Name = "Form1"
+        Me.Text = "IIS LogFile Viewer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
     Friend WithEvents btnOpenFile As Button
